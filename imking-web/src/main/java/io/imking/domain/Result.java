14e377f9c7143b161d19f898f062929d02fc81fc
GaryHuang <834865081@qq.com>
2018-02-05 19:58:32 +0800

    修改字段status

--- imking-web/src/main/java/io/imking/domain/Result.java
+++ imking-web/src/main/java/io/imking/domain/Result.java
@@ -14,31 +14,31 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
 public class Result<T> {
-	private int code;
+	private int status;
 	private String desc;
 	private T data;
 
 	public Result(ResultEnum resultEnum) {
-		this.code = resultEnum.getCode();
+		this.status = resultEnum.getCode();
 		this.desc = resultEnum.getDesc();
 	}
 
 	public Result(ResultEnum resultEnum, T data) {
-		this.code = resultEnum.getCode();
+		this.status = resultEnum.getCode();
 		this.desc = resultEnum.getDesc();
 		this.data = data;
 	}
 
 	public Result<T> ok(T data) {
 		this.data = data;
-		this.code = ResultEnum.SUCCESS.getCode();
+		this.status = ResultEnum.SUCCESS.getCode();
 		this.desc = ResultEnum.SUCCESS.getDesc();
 		return this;
 	}
 	
 	public Result<T> error(T data) {
 		this.data = data;
-		this.code = ResultEnum.SERVER_ERROR.getCode();
+		this.status = ResultEnum.SERVER_ERROR.getCode();
 		this.desc = ResultEnum.SERVER_ERROR.getDesc();
 		return this;
 	}
