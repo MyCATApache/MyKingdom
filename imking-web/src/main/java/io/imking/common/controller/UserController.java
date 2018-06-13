317cf88c81d54697282078c5fd7c8836423d8a7f
zhanyd <zhanyd@sina.com>
2018-06-13 16:07:45 +0800

    新增忘记密码发邮件功能

--- imking-web/src/main/java/io/imking/common/controller/UserController.java
+++ imking-web/src/main/java/io/imking/common/controller/UserController.java
@@ -1,5 +1,7 @@
 package io.imking.common.controller;
 
+import io.imking.common.services.MailService;
+import io.imking.utils.GenRandom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +22,18 @@ public class UserController {
      private UserService userService;
 	 @Autowired
 	 private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	MailService mailService;
 	 
 	 //获取用户信息
 	 @PostMapping("getUserInfo")
      public Result<ImkUserDTO> getUserInfo(Integer id){
-		 Result<ImkUserDTO> result =  userService.getUserInfo(id);
+		 Result<ImkUserDTO> result = new Result<ImkUserDTO>();
+		 ImkUserDTO user = userService.getUserInfo(id);
+		 if(user != null){
+			 result.setData(user);
+		 }
          return result;
 	 }
 	 //用户注册
@@ -46,4 +55,37 @@ public class UserController {
 			return result;
 	       
 	    }
+
+
+	/**
+	 * 找回密码
+	 * @param id 用户id
+	 * @return
+	 * @throws Exception
+	 * @author zhanyingda
+	 */
+	@PostMapping("/findPassward")
+	public Result findPassward(Integer id) {
+	 	//TODO 验证验证码
+		Result result = new Result();
+		try{
+			//获取邮箱
+			ImkUserDTO userDTO = userService.getUserInfo(id);
+			//随机生成密码
+			String passward = GenRandom.genRandomNum(6);
+			//更新密码
+			ImkUser user = new ImkUser();
+			user.setId(id);
+			user.setPwd(bCryptPasswordEncoder.encode(passward));
+			userService.updateByPrimaryKeySelective(user);
+			//发送邮件
+			mailService.sendSimpleMail(userDTO.getEmail(),"mykingdom密码找回","您的密码已重置为:" + passward);
+			result.ok("密码已发送到你邮箱");
+		}catch (Exception e) {
+			result.setStatus(ResultEnum.SERVER_ERROR.getCode());
+			result.setDesc("找回密码失败");
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
